a0b9c73dba31a7247d8d2128b80d665bf0cb4c10
Junghwan <hwane2000@gmail.com>
2026-01-11 21:12:16 +0900

    [add] #15 - 폰트 추가

---
+++ POTI-iOS/POTI-iOS/Presentation/Common/Extension/UIFonts+.swift
@@ -0,0 +1,36 @@
+//
+//  UIFonts+.swift
+//  POTI-iOS
+//
+//  Created by 박정환 on 1/11/26.
+//
+
+import UIKit
+
+extension UIFont {
+    enum FontType: String {
+        case bold = "Pretendard-Bold"
+        case medium = "Pretendard-Medium"
+        case semibold = "Pretendard-Semibold"
+        
+        var name: String {
+            return self.rawValue
+        }
+        
+        static func font(_ type: FontType, ofsize size: CGFloat) -> UIFont {
+            return UIFont(name: type.rawValue, size: size)!
+        }
+    }
+    
+    static let display_20b = FontManager.display20b.font
+    static let display_18b = FontManager.display18b.font
+    static let title_18sb  = FontManager.title18sb.font
+    static let body_16sb   = FontManager.body16sb.font
+    static let body_16m    = FontManager.body16m.font
+    static let body_14sb   = FontManager.body14sb.font
+    static let body_14m    = FontManager.body14m.font
+    static let caption_12m = FontManager.caption12m.font
+    static let caption_10m = FontManager.caption10m.font
+    static let button_16sb = FontManager.button16sb.font
+    static let button_14sb = FontManager.button14sb.font
+}
---
+++ POTI-iOS/POTI-iOS/Resource/Fonts/FontManager.swift
@@ -0,0 +1,61 @@
+//
+//  FontManager.swift
+//  POTI-iOS
+//
+//  Created by 박정환 on 1/11/26.
+//
+
+import UIKit
+
+public struct FontProperty {
+    let font: UIFont.FontType
+    let size: CGFloat
+    let lineHeight: CGFloat
+}
+
+public enum FontManager {
+    case display20b
+    case display18b
+    case title18sb
+    case body16sb
+    case body16m
+    case body14sb
+    case body14m
+    case caption12m
+    case caption10m
+    case button16sb
+    case button14sb
+
+    public var property: FontProperty {
+        switch self {
+        case .display20b:
+            return FontProperty(font: .bold, size: 20, lineHeight: 20 * 1.4)
+        case .display18b:
+            return FontProperty(font: .bold, size: 18, lineHeight: 18 * 1.4)
+        case .title18sb:
+            return FontProperty(font: .semibold, size: 18, lineHeight: 18 * 1.4)
+        case .body16sb:
+            return FontProperty(font: .semibold, size: 16, lineHeight: 16 * 1.5)
+        case .body16m:
+            return FontProperty(font: .medium, size: 16, lineHeight: 16 * 1.5)
+        case .body14sb:
+            return FontProperty(font: .semibold, size: 14, lineHeight: 14 * 1.5)
+        case .body14m:
+            return FontProperty(font: .medium, size: 14, lineHeight: 14 * 1.5)
+        case .caption12m:
+            return FontProperty(font: .medium, size: 12, lineHeight: 12 * 1.5)
+        case .caption10m:
+            return FontProperty(font: .medium, size: 10, lineHeight: 10 * 1.5)
+        case .button16sb:
+            return FontProperty(font: .semibold, size: 16, lineHeight: 16 * 1.5)
+        case .button14sb:
+            return FontProperty(font: .semibold, size: 14, lineHeight: 14 * 1.5)
+        }
+    }
+}
+
+public extension FontManager {
+    var font: UIFont {
+        UIFont.FontType.font(property.font, ofsize: property.size)
+    }
+}
