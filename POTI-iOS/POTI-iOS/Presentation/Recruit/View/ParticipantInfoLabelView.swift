b4d761cfd021f6fb3935d1d178e6974401cd2676
Wizzy <doitexactly@naver.com>
2026-01-15 23:54:00 +0900

    [feat] #38 - InfoLabel 뷰 생성

---
+++ POTI-iOS/POTI-iOS/Presentation/Recruit/View/ParticipantInfoLabelView.swift
@@ -0,0 +1,78 @@
+//
+//  ParticipantInfoLabelView.swift
+//  POTI-iOS
+//
+//  Created by 이서현 on 1/15/26.
+//
+import UIKit
+
+import SnapKit
+import Then
+
+final class ParticipantInfoLabelView: BaseView {
+    private let titleLabel = UILabel()
+    private let infoLabel = UILabel()
+    
+    // MARK: - Custom Method
+    
+    override func setStyle() {
+        titleLabel.do {
+            $0.textColor = .potiBlack
+            $0.font = PotiFontManager.body14sb.font
+        }
+        
+        infoLabel.do {
+            $0.textColor = .potiBlack
+            $0.font = PotiFontManager.body14m.font
+            $0.numberOfLines = 2
+        }
+    }
+    
+    override func setUI() {
+        addSubviews(
+            titleLabel,
+            infoLabel
+        )
+    }
+    
+    override func setLayout() {
+        titleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.leading.equalToSuperview()
+        }
+        infoLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(8)
+            $0.horizontalEdges.equalTo(titleLabel)
+            $0.bottom.equalToSuperview()
+        }
+    }
+    
+    // TODO: - Participant~Case에서 configure(model: ParticipantManageModel) 로 받아서 아래처럼 처리
+
+//    func configure(title: String, info: String) {
+//        titleLabel.text = title
+//        infoLabel.text = info
+//    }
+    
+    func configure(title: String, infos: [String]) { // 행간 정해지면 나연언니한테 물어보기!
+        titleLabel.setLabel(title, font: .body14sb)
+
+        let text = infos
+            .filter { !$0.isEmpty }
+            .joined(separator: "\n")
+
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = 6   // 디쟌쌤들께 물어보기 TODO!!
+
+        let attributedText = NSAttributedString(
+            string: text,
+            attributes: [
+                .font: PotiFontManager.body14m.font,
+                .foregroundColor: UIColor.potiBlack,
+                .paragraphStyle: paragraphStyle
+            ]
+        )
+
+        infoLabel.attributedText = attributedText
+    }
+}
