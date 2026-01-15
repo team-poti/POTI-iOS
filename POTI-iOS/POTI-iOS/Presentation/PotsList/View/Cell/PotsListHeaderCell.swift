5bdfb2b423e244fa32800908d128d153e449ba98
sumchive <haon0319@naver.com>
2026-01-15 19:25:26 +0900

    [feat] #35 - PotListHeaderCell 구현

---
+++ POTI-iOS/POTI-iOS/Presentation/PotsList/View/Cell/PotsListHeaderCell.swift
@@ -0,0 +1,115 @@
+//
+//  PotsListHeaderCell.swift
+//  POTI-iOS
+//
+//  Created by mandoo on 1/15/26.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+protocol PotsListHeaderCellDelegate: AnyObject {
+    func rightFilterButtonDidTap()
+    func leftFilterButtonDidTap()
+}
+
+final class PotsListHeaderCell: UICollectionReusableView {
+
+    // MARK: - Property
+
+    weak var delegate: PotsListHeaderCellDelegate?
+
+    // MARK: - UI Components
+
+    private let leftFilterButton = UIButton()
+    private let rightFilterButton = UIButton()
+
+    // MARK: - Initializer
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        setStyle()
+        setUI()
+        setLayout()
+        addTarget()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Custom Methods
+
+    private func setStyle() {
+        leftFilterButton.do {
+
+            // TODO: - 멤버선택 바텀시트 추가 후 변경 텍스트로 변경하기
+
+            $0.setTitle("멤버 선택", for: .normal)
+            $0.setTitleColor(.potiBlack, for: .normal)
+            $0.titleLabel?.font = PotiFontManager.body14m.font
+            $0.setImage(.icnArrowDownSm, for: .normal)
+            $0.setImage(.icnArrowUpSm, for: .selected)
+            $0.semanticContentAttribute = .forceRightToLeft
+        }
+        
+        rightFilterButton.do {
+
+            // TODO: - 정렬 바텀시트 추가 후 변경 텍스트로 변경하기
+
+            $0.setTitle("평점순", for: .normal)
+            $0.setTitleColor(.potiBlack, for: .normal)
+            $0.titleLabel?.font = PotiFontManager.body14m.font
+            $0.setImage(.icnArrowDownSm, for: .normal)
+            $0.setImage(.icnArrowUpSm, for: .selected)
+            $0.semanticContentAttribute = .forceRightToLeft
+        }
+    }
+
+    private func setUI() {
+        addSubviews(leftFilterButton, rightFilterButton)
+    }
+
+    private func setLayout() {
+        leftFilterButton.snp.makeConstraints {
+            $0.leading.top.equalToSuperview()
+        }
+        
+        rightFilterButton.snp.makeConstraints {
+            $0.trailing.top.equalToSuperview()
+        }
+    }
+
+    // MARK: - Private Method
+
+    private func addTarget() {
+        leftFilterButton.addTarget(self, action: #selector(leftFilterButtonTapped), for: .touchUpInside)
+        
+        rightFilterButton.addTarget(self, action: #selector(rightFilterButtonTapped), for: .touchUpInside)
+    }
+
+    //MARK: - Action
+    
+    @objc private func leftFilterButtonTapped() {
+        leftFilterButton.isSelected.toggle()
+        delegate?.leftFilterButtonDidTap()
+    }
+
+    @objc private func rightFilterButtonTapped() {
+        rightFilterButton.isSelected.toggle()
+        delegate?.rightFilterButtonDidTap()
+    }
+}
+
+// MARK: - Extension
+
+extension PotsListHeaderCell {
+    func configure() {
+
+        // TODO: - 필터링 바텀시트랑 연결하기
+
+    }
+}
