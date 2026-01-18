439ce2237e4d7ced24a276842fa3245df7be9e06
Wizzy <doitexactly@naver.com>
2026-01-18 15:10:46 +0900

    [fix] #38 - ManageModel -> Entity 수정

--- POTI-iOS/POTI-iOS/Presentation/Recruit/Model/ParticipantManageModel.swift
+++ POTI-iOS/POTI-iOS/Presentation/Recruit/Model/ParticipantManageModel.swift
@@ -18,7 +18,6 @@ struct ParticipantManageModel: Hashable {
     let waitPayCheckInfo: WaitPayCheckInfo? // nil이면 숨김
     let paidInfo: PaidInfo? // nil이면 숨김
     let shipInfo: ShipInfo? // nil이면 숨김
-    let completedInfo: CompletedInfo? // nil이면 숨김
 
     struct MemberRow: Hashable {
         let name: String
@@ -39,7 +38,113 @@ struct ParticipantManageModel: Hashable {
         let phoneText: String // "배송 시작"
         let trackingNumber: String // 운송장 번호 (nil 로 받고 내가 post)
     }
-    struct CompletedInfo: Hashable {
-        let trackingNumber: String
+}
+
+//Entity -> Model
+
+extension ManageEntity {
+    func toParticipantManageModels() -> [ParticipantManageModel] {
+        participants.map { $0.toParticipantManageModel() }
     }
 }
+
+extension ParticipantEntity {
+    func toParticipantManageModel() -> ParticipantManageModel {
+        let memberRows: [ParticipantManageModel.MemberRow] = priceInfo.memberPerPrices.map {
+            .init(name: $0.name, price: $0.price)
+        }
+        
+        let waitPayCheckInfo: ParticipantManageModel.WaitPayCheckInfo? = depositInfo.map {
+                    .init(
+                        depositorName: $0.depositorName,
+                        depositTimeText: $0.depositTime
+                    )
+                }
+
+        // PAID 상태 + depositInfo 존재 시에만 생성
+        let paidInfo: ParticipantManageModel.PaidInfo? =
+        (status == .paid ? depositInfo : nil)
+                .map {
+                    .init(
+                        depositorName: $0.depositorName,
+                        depositTimeText: $0.depositTime
+                    )
+                }
+
+        // shippingInfo가 있을 때만 ShipInfo 생성
+        let shipInfo: ParticipantManageModel.ShipInfo? = shippingInfo.map {
+            .init(
+                receiverName: $0.receiverName,
+                addressText: $0.address,
+                phoneText: $0.phone,
+                trackingNumber: $0.trackingNumber ?? ""
+            )
+        }
+
+        return ParticipantManageModel(
+            purchaseId: orderId,
+            profileImage: profileImage ?? "",
+            nickname: nickname,
+            memberTitle: memberNames,
+            participantstatus: status,
+            memberRows: memberRows,
+            shippingText: priceInfo.shippingName,
+            shippingPrice: priceInfo.shippingPrice,
+            totalPrice: priceInfo.totalPrice,
+            waitPayCheckInfo: waitPayCheckInfo,
+            paidInfo: paidInfo,
+            shipInfo: shipInfo
+        )
+    }
+}
+
+//
+//
+//extension ParticipantEntity {
+//    func toParticipantManageModel() -> ParticipantManageModel {
+//        let memberRows: [ParticipantManageModel.MemberRow] = priceInfo.memberPerPrices.map {
+//            .init(name: $0.name, price: $0.price)
+//        }
+//
+//        // MARK: - Sections by Status (map 기반)
+//
+//        // WAIT_PAY_CHECK 상태 + depositInfo 존재 시에만 생성
+//        let waitPayCheckInfo: ParticipantManageModel.WaitPayCheckInfo? =
+//            (status == .waitPayCheck ? depositInfo : nil)
+//                .map {
+//                    .init(
+//                        depositorName: $0.depositorName,
+//                        depositTimeText: $0.depositTime
+//                    )
+//                }
+//
+//        // TODO: 서버 상태가 확장되면 여기에서 paidInfo를 채우도록 분기
+//        // 현재 Entity에는 paid(입금 완료) 상태/필드가 없어서 우선 nil
+//        let paidInfo: ParticipantManageModel.PaidInfo? = nil
+//
+//        // shippingInfo가 있을 때만 ShipInfo 생성
+//        let shipInfo: ParticipantManageModel.ShipInfo? = shippingInfo.map {
+//            .init(
+//                receiverName: $0.receiverName,
+//                addressText: $0.address,
+//                phoneText: $0.phone,
+//                trackingNumber: $0.trackingNumber ?? ""
+//            )
+//        }
+//
+//        return ParticipantManageModel(
+//            purchaseId: orderId,
+//            profileImage: profileImage ?? "",
+//            nickname: nickname,
+//            memberTitle: memberNames,
+//            participantstatus: status,
+//            memberRows: memberRows,
+//            shippingText: priceInfo.shippingName,
+//            shippingPrice: priceInfo.shippingPrice,
+//            totalPrice: priceInfo.totalPrice,
+//            waitPayCheckInfo: waitPayCheckInfo,
+//            paidInfo: paidInfo,
+//            shipInfo: shipInfo
+//        )
+//    }
+//}
