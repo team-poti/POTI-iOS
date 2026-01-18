7f03005f19c0701ec50047d03cc33088ba0a68f5
Junghwan <hwane2000@gmail.com>
2026-01-19 00:14:37 +0900

    [feat] #54 - RegisterNoticeView 구현

--- POTI-iOS/POTI-iOS/Presentation/ProductRegister/View/RegisterNoticeView.swift
+++ POTI-iOS/POTI-iOS/Presentation/ProductRegister/View/RegisterNoticeView.swift
@@ -7,6 +7,43 @@
 
 import UIKit
 
-final class RegisterNoticeView: UIView {
-    
+final class RegisterNoticeView: BaseView {
+
+    // MARK: - Property
+
+    private var text: String = ""
+
+    // MARK: - UI Components
+
+    private let noticeLabel = UILabel()
+
+    // MARK: - Custom Method
+
+    func configure(text: String) {
+        self.text = text
+        noticeLabel.text = text
+    }
+
+    // MARK: - UI Setting
+
+    override func setStyle() {
+        backgroundColor = .clear
+
+        noticeLabel.do {
+            $0.numberOfLines = 0
+            $0.textAlignment = .left
+            $0.font = PotiFontManager.caption12m.font
+            $0.textColor = .gray800
+        }
+    }
+
+    override func setUI() {
+        addSubview(noticeLabel)
+    }
+
+    override func setLayout() {
+        noticeLabel.snp.makeConstraints {
+            $0.edges.equalToSuperview().inset(16)
+        }
+    }
 }
