1d8ffe45417fc82e59d3e7659716dff3e9c080a6
Wizzy <doitexactly@naver.com>
2026-01-19 06:49:00 +0900

    [feat] #63 - 모집자 tableView Cell 생성

---
+++ POTI-iOS/POTI-iOS/Presentation/MyPageJoin/View/Cell/JoinProgressStatusViewCell.swift
@@ -0,0 +1,101 @@
+//
+//  JoinProgressStatusViewCell.swift
+//  POTI-iOS
+//
+//  Created by 이서현 on 1/13/26.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class JoinProgressStatusViewCell: UITableViewCell {
+    
+    private let mockJoinProgressStatusModel: ProgressStatusModel = ProgressStatusModel(
+        potId: 1110,
+        role: .participant,
+        status: .recruiting
+    )
+    
+    // MARK: - Lifecycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.backgroundColor = .potiWhite
+        
+        setUI()
+        setStyle()
+        setLayout()
+        configure(model: mockJoinProgressStatusModel)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - UI Component
+    
+    private let progressTitleLabel = UILabel()
+    private let potStatusMessageView = StatusMessageView()
+    private let progressStatusBar = UIImageView()
+    private let divideView = UIView()
+
+    // MARK: - Custom Method
+    
+    private func setStyle() {
+        progressTitleLabel.do {
+            $0.setLabel("진행 상황", font: .body16sb)
+            $0.textColor = .potiBlack
+        }
+        
+        progressStatusBar.do {
+            $0.backgroundColor = .potiWhite
+            $0.contentMode = .scaleAspectFit
+        }
+        
+        divideView.do {
+            $0.backgroundColor = .gray100
+        }
+    }
+    
+    private func setUI() {
+        contentView.addSubviews(
+            progressTitleLabel,
+            potStatusMessageView,
+            progressStatusBar,
+            divideView
+        )
+    }
+    
+    private func setLayout() {
+        progressTitleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(20)
+            $0.leading.equalToSuperview().inset(16)
+        }
+        potStatusMessageView.snp.makeConstraints {
+            $0.top.equalTo(progressTitleLabel.snp.bottom).offset(20)
+            $0.horizontalEdges.equalToSuperview().inset(16)
+            $0.height.equalTo(CGFloat.dynamicH(45))
+        }
+        progressStatusBar.snp.makeConstraints {
+            $0.top.equalTo(potStatusMessageView.snp.bottom).offset(16)
+            $0.horizontalEdges.equalToSuperview().inset(24)
+            $0.height.equalTo(CGFloat.dynamicH(53))
+        }
+        divideView.snp.makeConstraints {
+            $0.top.equalTo(progressStatusBar.snp.bottom).offset(24)
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(CGFloat.dynamicH(8))
+            $0.bottom.equalToSuperview()
+        }
+    }
+    
+    func configure(model: ProgressStatusModel) {
+        
+        let messageText = model.status.statusText(role: model.role)
+        potStatusMessageView.configure(text: messageText)
+        
+        progressStatusBar.image = model.status.progressImage
+    }
+}
