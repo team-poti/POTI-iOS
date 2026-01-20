28ef50880d7c174d623ffb328ec253d5ca9a94e8
Wizzy <doitexactly@naver.com>
2026-01-20 16:05:26 +0900

    [fix] #20 - RecruitDetailViewModel 수정

--- POTI-iOS/POTI-iOS/Presentation/Recruit/ViewModel/RecruitDetailViewModel.swift
+++ POTI-iOS/POTI-iOS/Presentation/Recruit/ViewModel/RecruitDetailViewModel.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+import Combine
+
 enum UserRole {
     case host
     case participant
@@ -127,6 +129,57 @@ enum ParticipantStatus {
         }
 }
 
-final class RecruitDetailViewModel {
+final class RecruitDetailViewModel: BaseViewModelType {
     
+    // MARK: - Input
+    
+    enum Input {
+        case viewDidLoad
+    }
+    
+    // MARK: - Output
+    
+    struct Output {
+        let reloadData: AnyPublisher<Void, Never>
+    }
+    
+    // MARK: - Properties
+    
+    let output: Output
+    
+    // MARK: - Subject
+    
+    private let reloadDataSubject = PassthroughSubject<Void, Never>()
+    
+    // MARK: - Initializer
+    
+    init(useCase: GoodsListUseCase) {
+       //self.useCase = useCase
+        self.output = Output(
+            reloadData: reloadDataSubject.eraseToAnyPublisher()
+        )
+    }
+    
+    // MARK: - Action
+    
+    func action(_ trigger: Input) {
+        switch trigger {
+        case .viewDidLoad:
+            fetchRecruitDetail()
+        
+        }
+    }
+    // MARK: - Private Method
+    
+    private func fetchRecruitDetail() {
+//        Task {
+//            do {
+//                let data = try await useCase.execute()
+//                self.groupItems = data.toGroupItemModel()
+//                reloadDataSubject.send(())
+//            } catch {
+//                print("Error: \(error)")
+//            }
+//        }
+    }
 }
