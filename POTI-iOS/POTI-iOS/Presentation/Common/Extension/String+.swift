919c879fed388acea82722c654be8790700d351a
Yeonnies <eliyeon16@gmail.com>
2026-01-21 21:05:04 +0900

    [fix] #46 - 서버 로직 반영

--- POTI-iOS/POTI-iOS/Presentation/Common/Extension/String+.swift
+++ POTI-iOS/POTI-iOS/Presentation/Common/Extension/String+.swift
@@ -16,4 +16,22 @@ extension String {
         formatter.timeZone = TimeZone(identifier: "Asia/Seoul")
         return formatter.date(from: self)!
     }
+    
+    func toKoreanYMD() -> String? {
+        let inputFormatter = DateFormatter()
+        inputFormatter.dateFormat = "yyyy-MM-dd"
+        inputFormatter.locale = Locale(identifier: "ko_KR")
+        inputFormatter.timeZone = TimeZone(identifier: "Asia/Seoul")
+        
+        guard let date = inputFormatter.date(from: self) else {
+            return nil
+        }
+        
+        let outputFormatter = DateFormatter()
+        outputFormatter.dateFormat = "yyyy년 M월 d일"
+        outputFormatter.locale = Locale(identifier: "ko_KR")
+        outputFormatter.timeZone = TimeZone(identifier: "Asia/Seoul")
+        
+        return outputFormatter.string(from: date)
+    }
 }
