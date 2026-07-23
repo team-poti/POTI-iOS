47b3eaa577554cad93ab0e96da8b87fb0d0a1c93
sumchive <haon0319@naver.com>
2026-07-23 22:17:43 +0900

    [chore] #151 - 팟 옵션 임시 Mock 데이터 제거

--- POTI-iOS/POTI-iOS/Presentation/PotOption/Model/MembersModel.swift
+++ POTI-iOS/POTI-iOS/Presentation/PotOption/Model/MembersModel.swift
@@ -10,12 +10,3 @@ struct MemberModel {
     let memberName: String
     let memberOptionPrice: Int
 }
-
-let mockMembers: [MemberModel] = .init([
-    .init(memberOptionId: 1, memberName: "원영", memberOptionPrice: 17000),
-    .init(memberOptionId: 2, memberName: "레이", memberOptionPrice: 9000),
-    .init(memberOptionId: 3, memberName: "이서", memberOptionPrice: 17000),
-    .init(memberOptionId: 4, memberName: "유진", memberOptionPrice: 15000),
-    .init(memberOptionId: 5, memberName: "가을", memberOptionPrice: 9000),
-    .init(memberOptionId: 6, memberName: "리즈", memberOptionPrice: 10000),
-])
