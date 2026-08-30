1422328c1b6d7abc9d10efd099529dd3c24efecc
sumchive <haon0319@naver.com>
2026-08-30 23:43:10 +0900

    [refactor] #181 - Layer에 맞게 분리

--- POTI-iOS/POTI-iOS/Core/Service/PushNotification/FCMTokenSyncService.swift
+++ POTI-iOS/POTI-iOS/Core/Service/PushNotification/FCMTokenSyncService.swift
@@ -7,50 +7,5 @@
 
 protocol FCMTokenSyncService: AnyObject {
     func synchronize(token: String?) async
-    func deleteToken() async
-}
-
-final class DefaultFCMTokenSyncService: FCMTokenSyncService {
-
-    // MARK: - Properties
-
-    private let registerFCMTokenUseCase: RegisterFCMTokenUseCase
-    private let deleteFCMTokenUseCase: DeleteFCMTokenUseCase
-    private let tokenStore: FCMTokenStore
-
-    // MARK: - Initializer
-
-    init(registerFCMTokenUseCase: RegisterFCMTokenUseCase, deleteFCMTokenUseCase: DeleteFCMTokenUseCase, tokenStore: FCMTokenStore) {
-        self.registerFCMTokenUseCase = registerFCMTokenUseCase
-        self.deleteFCMTokenUseCase = deleteFCMTokenUseCase
-        self.tokenStore = tokenStore
-    }
-
-    // MARK: - Public Methods
-
-    func synchronize(token: String? = nil) async {
-        if let token {
-            tokenStore.save(token)
-        }
-
-        guard KeychainManager.getAccessToken() != nil,
-              let storedToken = tokenStore.token else { return }
-
-        do {
-            try await registerFCMTokenUseCase.execute(token: storedToken)
-        } catch {
-            PotiLogger.error(error)
-        }
-    }
-
-    func deleteToken() async {
-        guard let token = tokenStore.token else { return }
-
-        do {
-            try await deleteFCMTokenUseCase.execute(token: token)
-            tokenStore.clear()
-        } catch {
-            PotiLogger.error(error)
-        }
-    }
+    func deleteToken() async throws
 }
