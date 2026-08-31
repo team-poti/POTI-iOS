cf89f6e491aafb5707acda8f2bc3f1672b53574e
sumchive <haon0319@naver.com>
2026-08-31 15:23:46 +0900

    [fix] #194 - 로그인 세션 기반 푸시 알림 권한 안내 처리

--- POTI-iOS/POTI-iOS/Presentation/Notification/Coordinator/PushNotificationPermissionCoordinator.swift
+++ POTI-iOS/POTI-iOS/Presentation/Notification/Coordinator/PushNotificationPermissionCoordinator.swift
@@ -6,109 +6,161 @@
 //
 
 import UIKit
+import UserNotifications
 
+@MainActor
 final class PushNotificationPermissionCoordinator {
-    
+
     // MARK: - Properties
-    
+
     private let viewModel: NotificationSettingViewModel
     private let permissionService: PushNotificationPermissionService
+    private let fetchNotificationSettingsUseCase: FetchNotificationSettingsUseCase
     private var isWaitingForSystemSettings = false
     private var pendingPermissionAllowedAction: (() -> Void)?
-    
+    private var didEvaluateCurrentSession = false
+    private var isEvaluating = false
+    private var isModalPresented = false
+
     // MARK: - Initializer
-    
-    init(viewModel: NotificationSettingViewModel, permissionService: PushNotificationPermissionService) {
+
+    init(viewModel: NotificationSettingViewModel, permissionService: PushNotificationPermissionService,
+         fetchNotificationSettingsUseCase: FetchNotificationSettingsUseCase) {
         self.viewModel = viewModel
         self.permissionService = permissionService
-        
+        self.fetchNotificationSettingsUseCase = fetchNotificationSettingsUseCase
+
         NotificationCenter.default.addObserver(self, selector: #selector(applicationDidBecomeActive),
                                                name: UIApplication.didBecomeActiveNotification, object: nil)
     }
-    
+
     deinit {
         NotificationCenter.default.removeObserver(self)
     }
-    
+
     // MARK: - Private Methods
-    
+
     private func requestSystemPermission(whenPermissionAllowed: @escaping () -> Void) {
         Task { [weak self] in
             guard let self else { return }
             let result = await permissionService.requestPermissionOrOpenSettings()
-            await handlePermissionResult(result, whenPermissionAllowed: whenPermissionAllowed)
+            handlePermissionResult(result, whenPermissionAllowed: whenPermissionAllowed)
         }
     }
-    
-    @MainActor
+
     private func handlePermissionResult(_ result: PushNotificationPermissionResult,
                                         whenPermissionAllowed: @escaping () -> Void) {
         switch result {
         case .granted:
             whenPermissionAllowed()
         case .denied:
-            viewModel.action(.setAllNotifications(isEnabled: false))
+            break
         case .openedSettings:
             isWaitingForSystemSettings = true
             pendingPermissionAllowedAction = whenPermissionAllowed
-            viewModel.action(.setAllNotifications(isEnabled: false))
         }
     }
 
-    private func showPermissionModal(in view: UIView, whenPermissionAllowed: @escaping () -> Void) {
+    private func showPermissionModal(in view: UIView, onAllow: @escaping () -> Void) {
+        guard !isModalPresented else { return }
+        isModalPresented = true
+
         let modalView = PushNotificationPermissionModalView()
         modalView.onTapAllow = { [weak self] in
-            self?.requestSystemPermission(whenPermissionAllowed: whenPermissionAllowed)
+            self?.isModalPresented = false
+            onAllow()
         }
         modalView.onTapLater = { [weak self] in
-            Task { @MainActor in
-                self?.viewModel.action(.setAllNotifications(isEnabled: false))
-            }
+            self?.isModalPresented = false
         }
         modalView.show(in: view)
     }
 
+    private func handlePermissionModalAllow(systemPermissionAllowed: Bool, serverPermissionAllowed: Bool) {
+        let enableServerNotificationIfNeeded = { [weak self] in
+            guard !serverPermissionAllowed else { return }
+            self?.viewModel.action(.setAllNotifications(isEnabled: true))
+        }
+
+        if systemPermissionAllowed {
+            enableServerNotificationIfNeeded()
+        } else {
+            requestSystemPermission(whenPermissionAllowed: enableServerNotificationIfNeeded)
+        }
+    }
+
+    private static func isPermissionAllowed(_ status: UNAuthorizationStatus) -> Bool {
+        switch status {
+        case .authorized, .provisional, .ephemeral:
+            return true
+        default:
+            return false
+        }
+    }
+
     // MARK: - Public Methods
 
-    func showPermissionModal(in view: UIView) {
-        showPermissionModal(in: view) { [weak self] in
-            Task { @MainActor in
-                self?.viewModel.action(.setAllNotifications(isEnabled: true))
+    func evaluatePermissionIfNeeded(in view: UIView) {
+        guard KeychainManager.getAccessToken() != nil else {
+            didEvaluateCurrentSession = false
+            isEvaluating = false
+            return
+        }
+
+        guard !didEvaluateCurrentSession, !isEvaluating else { return }
+        isEvaluating = true
+
+        Task { [weak self] in
+            guard let self else { return }
+
+            do {
+                let status = await permissionService.authorizationStatus()
+                let settings = try await fetchNotificationSettingsUseCase.execute()
+                let systemPermissionAllowed = Self.isPermissionAllowed(status)
+                let serverPermissionAllowed = settings.isTradeNotificationEnabled || settings.isEventNotificationEnabled
+
+                isEvaluating = false
+                didEvaluateCurrentSession = true
+
+                guard !systemPermissionAllowed || !serverPermissionAllowed else { return }
+                showPermissionModal(in: view) { [weak self] in
+                    self?.handlePermissionModalAllow(systemPermissionAllowed: systemPermissionAllowed,
+                                                     serverPermissionAllowed: serverPermissionAllowed)
+                }
+            } catch {
+                isEvaluating = false
+                PotiLogger.error(error)
             }
         }
     }
-    
+
     func handleEnablingNotification(in view: UIView, whenPermissionAllowed: @escaping () -> Void) {
         Task { [weak self] in
             guard let self else { return }
-            
+
             if await permissionService.isPermissionAllowed() {
-                await MainActor.run { whenPermissionAllowed() }
+                whenPermissionAllowed()
             } else {
-                await MainActor.run {
-                    self.showPermissionModal(in: view, whenPermissionAllowed: whenPermissionAllowed)
+                showPermissionModal(in: view) { [weak self] in
+                    self?.requestSystemPermission(whenPermissionAllowed: whenPermissionAllowed)
                 }
             }
         }
     }
-    
+
     // MARK: - Action
-    
+
     @objc private func applicationDidBecomeActive() {
         guard isWaitingForSystemSettings else { return }
         isWaitingForSystemSettings = false
-        
+
         Task { [weak self] in
             guard let self else { return }
             let isAllowed = await permissionService.isPermissionAllowed()
-            await MainActor.run {
-                if isAllowed {
-                    self.pendingPermissionAllowedAction?()
-                } else {
-                    self.viewModel.action(.setAllNotifications(isEnabled: false))
-                }
-                self.pendingPermissionAllowedAction = nil
+            if isAllowed {
+                pendingPermissionAllowedAction?()
             }
+            pendingPermissionAllowedAction = nil
         }
     }
 }
